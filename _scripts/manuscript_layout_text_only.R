c7966764420863c3956594e3ae3f28b0a66bc155
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2020-10-07 15:20:53 +0200

    adjusting spacing

--- _scripts/manuscript_layout_text_only.R
+++ _scripts/manuscript_layout_text_only.R
@@ -3,11 +3,11 @@ create_content <- function() {
   cat("<div class='row manuscript-photo-and-text'>")
   
   # create left column
-  cat('<div class="col-sm-2">')
+  cat('<div class="col-sm-3">')
   cat('<h2>No manuscript images available at the moment.</h2>')
   cat("</div>") # close column
   
-  cat('<div class="col-sm-10">')
+  cat('<div class="col-sm-9">')
   
   print( htmltools::tagList(datatable(data_text,
                                       rownames = FALSE, 
