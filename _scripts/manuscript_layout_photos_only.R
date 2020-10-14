7f8869250bed8c4236ca885a07dad8206967f86c
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2020-10-14 09:56:26 +0200

    use another panzoom library for zooming with buttons

--- _scripts/manuscript_layout_photos_only.R
+++ _scripts/manuscript_layout_photos_only.R
@@ -5,7 +5,13 @@ create_content <- function(photoid, zoomid, photo_path) {
   # create left column
   cat('<div class="col-sm-9 manuscript-photo">')
   
-  cat(str_c('<div id="', zoomid, '">'))
+  cat('<div class="zoomButtons">')
+  cat(str_c('<button id="zoomIn', zoomid, '">Zoom in</button>'))
+  cat(str_c('<button id="zoomOut', zoomid, '">Zoom out</button>'))
+  cat(str_c('<button id="zoomReset', zoomid, '">Reset</button>'))
+  cat('</div>')
+  
+  cat(str_c('<div class="panzoomContainer" id="', zoomid, '">'))
   cat(str_c('<img src="', photo_path, '" class="img-fluid">'))
   cat('</div>') # close zoom container
   
@@ -24,8 +30,18 @@ initiate_zoom_effect <- function(photo_info_tibble) {
   # create row
   cat('<script>')
   
-  map(photo_info_tibble$zoomid,
-      ~cat(str_c('panzoom(document.querySelector("#', .x, '"));\n')))
+  photo_info_tibble %>% 
+    glue_data('
+const elem{zoomid} = document.querySelector("#{zoomid}");
+const panzoom{zoomid} = Panzoom(elem{zoomid});
+const zoomInButton{zoomid} = document.querySelector("#zoomIn{zoomid}");
+const zoomOutButton{zoomid} = document.querySelector("#zoomOut{zoomid}");
+const resetButton{zoomid} = document.querySelector("#zoomReset{zoomid}");
+zoomInButton{zoomid}.addEventListener("click", panzoom{zoomid}.zoomIn);
+zoomOutButton{zoomid}.addEventListener("click", panzoom{zoomid}.zoomOut);
+resetButton{zoomid}.addEventListener("click", panzoom{zoomid}.reset);\n
+') %>% 
+    cat()
   
   cat('</script>')
 }
