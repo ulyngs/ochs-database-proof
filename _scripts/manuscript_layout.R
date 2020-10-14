27cfbeee4af29fda9448e45923c21d51c0f81d7a
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2020-10-14 16:21:20 +0200

    implement solution for stacking text on photos

--- _scripts/manuscript_layout.R
+++ _scripts/manuscript_layout.R
@@ -6,9 +6,11 @@ create_content <- function(photoid, zoomid, photo_path, cols_hide) {
   cat('<div class="col-sm-6 manuscript-photo">')
   
   cat('<div class="zoomButtons">')
-  cat(str_c('<button id="zoomIn', zoomid, '">Zoom in</button>'))
-  cat(str_c('<button id="zoomOut', zoomid, '">Zoom out</button>'))
-  cat(str_c('<button id="zoomReset', zoomid, '">Reset</button>'))
+  cat(str_c('<button id="zoomIn', zoomid, '"><i class="fa fa-search-plus"></i></button>'))
+  cat(str_c('<button id="zoomOut', zoomid, '"><i class="fa fa-search-minus"></i></button>'))
+  cat(str_c('<button id="zoomReset', zoomid, '">Reset zoom</button>'))
+  cat(str_c('<button onclick="hideHeader()">Hide header</button>'))
+  cat(str_c('<button onclick="resetTransform()">Reset text</button>'))
   cat('</div>')
   
   cat(str_c('<div class="panzoomContainer" id="', zoomid, '">'))
@@ -25,8 +27,9 @@ create_content <- function(photoid, zoomid, photo_path, cols_hide) {
     select(chapter, verse, transliteration, starts_with("translation_"), everything())
   
   print( htmltools::tagList(datatable(current_table,
+                                      elementId = str_c(zoomid, "DT"),
                                       rownames = FALSE, 
-                                      escape = FALSE, 
+                                      escape = FALSE,
                                       class = "row-border",
                                       extensions = c('ColReorder', 'Buttons'),
                                       options = list(
@@ -36,6 +39,7 @@ create_content <- function(photoid, zoomid, photo_path, cols_hide) {
                                         scrollY = '600px',
                                         scrollX = TRUE,
                                         paging = FALSE,
+                                        info = FALSE,
                                         columnDefs = list(list(visible=FALSE, targets=cols_hide))))) )
   
   cat("</div>") # close column
@@ -44,9 +48,9 @@ create_content <- function(photoid, zoomid, photo_path, cols_hide) {
 }
 
 initiate_zoom_effect <- function(photo_info_tibble) {
+  # enable zooming of photos
   cat("<script src='/js/panzoom.js'></script>")
   
-  # create row
   cat('<script>')
   
   photo_info_tibble %>% 
@@ -61,6 +65,26 @@ zoomOutButton{zoomid}.addEventListener("click", panzoom{zoomid}.zoomOut);
 resetButton{zoomid}.addEventListener("click", panzoom{zoomid}.reset);\n
 ') %>% 
     cat()
+  
+  cat('</script>')
+  
+  # enabling zooming of text
+  cat('<script src="https://unpkg.com/panzoom@9.2.4/dist/panzoom.min.js"></script>')
+  
+  cat('<script>')
+  
+  photo_info_tibble %>%
+    glue_data('
+var element{{zoomid}} = document.querySelector("#{{zoomid}}DT");\n
+var instance = panzoom(element{{zoomid}}, { zoomDoubleClickSpeed: 1, filterKey: function(/* e, dx, dy, dz */) { return true; }, beforeWheel: function(e) { var shouldIgnore = !e.altKey; return shouldIgnore; }, beforeMouseDown: function(e) { var shouldIgnore = !e.altKey; return shouldIgnore; } });\n
+', .open = "{{", .close = "}}") %>%
+    cat()
+  
+  # create function to hide text header
+  cat('function hideHeader() {$(".dataTables_scrollHead").toggleClass("hide-on-pan"); $(".dt-buttons").toggleClass("hide-on-pan");$(".dataTables_filter").toggleClass("hide-on-pan");}')
+  
+  # create function to reset text position
+  cat('function resetTransform() { var el = document.querySelectorAll(".html-widget"); for (var i = 0; i < el.length; i++) { el[i].style.transform = "matrix(1, 0, 0, 1, 0, 0)"; } }')
   
   cat('</script>')
 }
