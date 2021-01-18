d4ede22c947534b346d9ab17523eadbc52ccc2af
ULRIK LYNGS <ulrik.lyngs@cs.ox.ac.uk>
2021-01-18 12:20:19 +0100

    remove full width button

--- _scripts/manuscript_layout_text_only.R
+++ _scripts/manuscript_layout_text_only.R
@@ -8,7 +8,6 @@ create_content <- function(cols_hide, text_only_version) {
   cat('<div class="col-sm-2">')
   
   cat('<div class="zoomButtons">')
-  cat('<button onclick="fullWidthTable()">Change table width</button>')
   insert_dictionary_dropdown()
   cat('</div>')
   
@@ -33,22 +32,3 @@ create_overlay_functions <- function(){
   cat('</script>')
 }
 
-create_full_width_function <- function(){
-  cat('<script>
-function fullWidthTable() {
-var tableWidget = document.querySelector(".manuscript-text .datatables");
-var table = document.querySelector(".manuscript-text table.display.dataTable.no-footer");
-var scrollHead = (".manuscript-text .dataTables_scrollHeadInner");
-
-var currentWidth = tableWidget.style.width;
-
-if (currentWidth !== "100%") {
-    tableWidget.style.width = "100%";
-    table.style.display = "100%";
-    scrollHead.style.display = "100%";
-  } else {
-    tableWidget.style.width = "960px";
-  }
-}
-</script>')
-}
